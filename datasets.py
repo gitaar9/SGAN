1fd73f960287b488b977fd38632722acb55fb7da
gitaar9 <thijs.eker@gmail.com>
2021-04-19 21:29:47 +0200

    new jobscript/curriclum/dataset for shapenet ships

--- datasets.py
+++ datasets.py
@@ -36,7 +36,7 @@ class ShapenetShips(CelebA):
         super().__init__(img_size)
 
         #self.data = glob.glob('/home/gitaar9/AI/TNO/shapenet_renderer/ship_renders/*/*/rgb/*.png')
-        self.data = glob.glob('/data/s2576597/graf_datasets/ship_renders/*/*/rgb/*.png')
+        self.data = glob.glob('/scratch/s2576597/graf_datasets/ship_renders_train_upper_hemisphere_30_fov/*/rgb/*.png')
         self.transform = transforms.Compose(
                     [transforms.Resize(256), transforms.CenterCrop(256), transforms.ToTensor(), transforms.Normalize([0.5], [0.5]), transforms.RandomHorizontalFlip(p=0.5), transforms.Resize((img_size, img_size), interpolation=0)])
 
